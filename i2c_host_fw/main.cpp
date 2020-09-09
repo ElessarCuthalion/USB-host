6a56b8fe42c13e8b5d55f38bad24936866480ce5
Eldalim\Elessar <elessarcuthalion@mail.ru>
2020-09-09 11:52:15 +0200

    + def

--- i2c_host_fw/main.cpp
+++ i2c_host_fw/main.cpp
@@ -4,11 +4,24 @@
 #include "uart.h"
 #include "shell.h"
 #include "kl_lib.h"
-//#include "led.h"
-//#include "Sequences.h"
-//#include "radio_lvl1.h"
-//#include "usb_cdc.h"
+
+#define I2C_EN
+//#define USB_EN
+//#define RGB_EN
+//#define RADIO_EN
+#ifdef I2C_EN
 #include "kl_i2c.h"
+#endif
+#ifdef RADIO_EN
+#include "radio_lvl1.h"
+#endif
+#ifdef RGB_EN
+#include "led.h"
+#include "Sequences.h"
+#endif
+#ifdef USB_EN
+#include "usb_cdc.h"
+#endif
 
 #define VL53L1_api_FULL
 //#define VL53L1_api_LITE
@@ -45,8 +58,9 @@ void ITask();
 #define CheckMeasurePeriod_MS 100
 
 const PinOutput_t PillPwr {PILL_PWR_PIN};
-//LedRGB_t Led { LED_R_PIN, LED_G_PIN, LED_B_PIN };
-
+#ifdef RGB_EN
+LedRGB_t Led { LED_R_PIN, LED_G_PIN, LED_B_PIN };
+#endif
 TmrKL_t MeasTMR {MS2ST(CheckMeasurePeriod_MS), evtIdCheckMeasure, tktPeriodic};
 #endif
 
@@ -66,20 +80,27 @@ int main(void) {
     Uart.Init();
     Printf("\r%S %S\r", APP_NAME, XSTRINGIFY(BUILD_TIME));
     Clk.PrintFreqs();
-
-//    Led.Init();
-//    Led.StartOrRestart(lsqStart);
-
+#ifdef RGB_EN
+    Led.Init();
+    Led.StartOrRestart(lsqStart);
+#endif
+#ifdef I2C_EN
     PillPwr.Init();
     PillPwr.SetHi();
     chThdSleepMilliseconds(300);
-
     i2c2.Init();
-
-//    UsbCDC.Init();
-//    Clk.EnableCRS();
-//    Clk.SelectUSBClock_HSI48();
-//    UsbCDC.Connect();
+#endif
+#ifdef USB_EN
+    UsbCDC.Init();
+    Clk.EnableCRS();
+    Clk.SelectUSBClock_HSI48();
+    UsbCDC.Connect();
+#endif
+#ifdef RADIO_EN
+    if(Radio.Init() == retvOk) {
+        Printf("Radio Ok\r");
+    }
+#endif
 
 	uint8_t Result = retvOk;
 #ifdef VL53L1_api_FULL
@@ -137,7 +158,9 @@ void ITask() {
         switch(Msg.ID) {
             case evtIdUsbNewCmd:
             case evtIdShellCmd:
-//                Led.StartOrRestart(lsqUSBCmd); // After that, falling throug is intentional
+#ifdef RGB_EN
+                Led.StartOrRestart(lsqUSBCmd); // After that, falling throug is intentional
+#endif
                 OnCmd((Shell_t*)Msg.Ptr);
                 ((Shell_t*)Msg.Ptr)->SignalCmdProcessed();
                 break;
@@ -177,7 +200,7 @@ void ITask() {
 #endif
 			    break;
 
-#if 0 // ======= USB =======
+#ifdef USB_EN // ======= USB =======
             case evtIdUsbConnect:
                 Printf("USB connect\r");
                 Clk.EnableCRS();
@@ -223,7 +246,7 @@ void OnCmd(Shell_t *PShell) {
     if(PCmd->NameIs("Ping")) {
         PShell->Ack(retvOk);
     }
-#if 0
+#ifdef I2C_EN0
     else if(PCmd->NameIs("help")) {
         PShell->Print("\r\n%S %S\r\n"
                 "Commands:\r\n"
@@ -234,11 +257,9 @@ void OnCmd(Shell_t *PShell) {
                 "W and WR return Ack 1 in case of communication error.\r\n",
                 APP_NAME, XSTRINGIFY(BUILD_TIME));
     }
-
     else if(PCmd->NameIs("Scan")) {
         i2c2.ScanBus(PShell);
     }
-
     // W <Addr> <Len <= 108 > (Data1, Data2, ..., DataLen)
     else if(PCmd->NameIs("W")) {
         uint8_t Addr, Len, Data[RW_LEN_MAX];
@@ -260,7 +281,6 @@ void OnCmd(Shell_t *PShell) {
         }
         else PShell->Ack(retvCmdError);
     }
-
     // WriteRead: WR <Addr> <LenW> <LenR> (Data1, Data2, ..., DataLen)
     else if(PCmd->NameIs("WR")) {
         uint8_t Addr, LenW, LenR, Data[RW_LEN_MAX];
@@ -287,6 +307,7 @@ void OnCmd(Shell_t *PShell) {
         else PShell->Ack(retvCmdError);
     }
 #endif
+
     else if(PCmd->NameIs("ChangeInterruptPolarity")) {
 #ifdef VL53L1_my
     	static VLInterruptPolarity_t IntPol = ipLow;
